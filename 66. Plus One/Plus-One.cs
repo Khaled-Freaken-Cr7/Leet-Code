6e1a1478530b85eb70961ba7b84fb46b7bf22ab4
Khaled Abdelfattah Hefzy <khassaan779@gmail.com>
2025-08-06 20:43:33 +0300

    [66] [Time Beats: 100.00%] [Memory Beats: 36.26%] - LeetPush

---
+++ 66. Plus One/Plus-One.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int[] PlusOne(int[] digits) {
+        for(int i=digits.Length-1;i>=0;i--)
+        {
+            if(digits[i]!=9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        int[] arr = new int [digits.Length+1];
+        arr[0] = 1;
+        return arr;
+    }
+}
