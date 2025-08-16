396e047b097dc1cc09274b3a55c2114ae1f0f45e
Khaled Abdelfattah Hefzy <khassaan779@gmail.com>
2025-08-16 17:27:16 +0300

    [80] [Time Beats: 58.32%] [Memory Beats: 27.30%] - LeetPush

---
+++ Remove Duplicates from Sorted Array II/Remove-Duplicates-from-Sorted-Array-II.cs
@@ -0,0 +1,16 @@
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        int i = 0;
+        foreach (int num in nums)
+        {
+            if (i < 2 || num != nums[i - 2])
+            {
+                nums[i] = num;
+                i++;
+            }
+        }
+        return i;
+    }
+}
