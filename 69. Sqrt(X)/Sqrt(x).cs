b1d11ac21eb39ce6b866759270fa6f8a6fdbb752
Khaled Abdelfattah Hefzy <khassaan779@gmail.com>
2025-08-16 17:54:05 +0300

    [69] [Time Beats: 100.00%] [Memory Beats: 61.05%] - LeetPush

---
+++ 69. Sqrt(X)/Sqrt(x).cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int MySqrt(int x)
+    {
+        if (x < 2) return x;
+        int l = 1, r = x / 2;
+        while (l <= r) 
+        {
+            int mid = l + (r - l) / 2;
+            long sq = (long) mid * mid;
+            if (sq == x) 
+            {
+                return mid;
+            }
+            else if (sq < x)
+            {
+                l = mid + 1;
+            } 
+            else 
+            {
+                r = mid - 1;
+            }
+        }
+        return r;       
+    }
+}
